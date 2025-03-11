608344493c27fc17150a4d524a05a20d45bed005
Sayan Das <sayan.nanoor@gmail.com>
2025-03-11 23:21:03 +0530

    Update Final_test.py

--- Final_test.py
+++ Final_test.py
@@ -194,7 +194,7 @@ df_data = pd.DataFrame(data)
 #svm_model = load_model()
 """
 # Prepare the input for the SVM model
-input_data = pd.DataFrame({
+df_data = pd.DataFrame({
     "Clickbait_Score" : [Clickbait_Score],
     "Hyperbole_Score": [Hyperbole_Score],
     "HC_Sentiment": [HC_Sentiment],
